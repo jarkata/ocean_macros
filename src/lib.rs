fa99dd673e04f3c847c22757282b89515d1a987d
Jarkata <jarkata@foxmail.com>
2025-02-19 22:18:27 +0800

    init ocean_macros

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod field_ordered;
 mod sql_columns;
 
 use crate::field_ordered::ordered_fields;
+use crate::sql_columns::all_columns;
 use proc_macro::TokenStream;
 
 /**
@@ -10,4 +11,9 @@ use proc_macro::TokenStream;
 #[proc_macro_attribute]
 pub fn to_ordered_vec(_attr: TokenStream, item: TokenStream) -> TokenStream {
     ordered_fields(item)
+}
+
+#[proc_macro_attribute]
+pub fn all_columns(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    all_columns(_attr, item)
 }
